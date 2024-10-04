755439102e16fc8dfbbf61b6db1cd27ef3a73359
JHY06 <ouou1326@gmail.com>
2024-10-04 15:04:37 +0900

    gnb 구현

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const gnbMenu = document.querySelector(".gnb");
-    gnbMenu.addEventListener("click", function () {
-        gnbMenu.classList.toggle("expanded");
-    });
-});
